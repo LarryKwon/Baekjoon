1c55ef902fbd344b9b81db435028b7fa96ec006d
laryrkwon <koallarry11@kaist.ac.kr>
2022-03-25 01:22:08 +0000

    input: the number of participants and their ability matrix, which express how much ability that the team can get when it contains the pair of teamMates(Sij)
    Therefore, I need three data structures
    first, vector<vector<int>> save the ability matrix,
    second, the vector<bool> teamMates,
    third, the set of diff of abilities which each team get
    
    Divide & Conquer
    1. make the 2-degree vector by the input, ability matrix
    2. backtracking on the size of teamMates, and exit when the teamMates is same as teamMates.size()/2
    this mark the teamMates as true, false, in turn, teamMates divides into two team. Also, this marks should be by ascending permutations in that 1,2,3 and 1,3,2 is same.
    3. in the exit condition, we make the vector of each team, by traversing the teamMates vector which is marked by bactracking.
    4. after that, we have to make total ability points by backtracking on each team.
    6. in that backtracking, set the exit condition as the total number of backtracking is 2, because we can accumulate the ability
    by grouping the teamMates in the team by 2.
    7. in exit condition. we return the ability points of each pairs. and we should sum it and return after for-loop of backtracking.

--- 14889.cpp
+++ 14889.cpp
@@ -1,125 +1,139 @@
-// #include<iostream>
-// #include<sstream>
-// #include<fstream>
-// #include<string>
-// #include<algorithm>
-// #include<cmath>
-// #include<stdexcept>
-// #include<memory>
-// #include<vector>
-// #include<list>
-// #include<map>
-// #include<set>
-// #include<numeric>
+#include<iostream>
+#include<sstream>
+#include<fstream>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<stdexcept>
+#include<memory>
+#include<vector>
+#include<list>
+#include<map>
+#include<set>
+#include<numeric>
 
-// using namespace std;
+using namespace std;
 
-// static set<int> diff;
+/*
+pseudo code
+필요한 자료
+- Sij를 담을 2차원 벡터
+- team원 정보를 담을 N/2짜리 1차원 벡터
+- 전체 명수를 담을 N
+- 각 팀에서 점수의 합을 계산할 s_ability, t_ability
+- 각팀의 점수차이를 저장할 set 자료구조
+*/
 
-// static int n;
+static set<int> ability_diff;
 
-// void cal_point(vector<pair<int,bool>> team, int nth, int& point, vector<int>& comb_team,vector<vector<int>>& points,int num){
-//   if(nth == 2){
-//     int first = comb_team[0];
-//     int second = comb_team[1];
-//     point += points[first-1][second-1];
-//     point += points[second-1][first-1];
-//     return ;
-//   }
-//   else{
-//     for(int i=num; i<n/2;i++){
-//       if(team[i].second == false){
-//         team[i].second = true;
-//         comb_team[nth] = team[i].first;
-//         cal_point(team,nth+1,point,comb_team,points,i+1);
-//         team[i].second = false;
-//         comb_team[nth] = 0;
-//       }
-//     }
-//   }
-// }
+int cal_ability(vector<pair<int,bool> >& team, int nth, int sum, vector<vector<int> >& points, vector<int>& comb){
+    if(nth == 2){
+        // cout << "comb: " << comb[0] << comb[1] << " points: " << points[comb[0]][comb[1]] << '\n';
+        return points[comb[0]][comb[1]];
+    }else{
+        for(int i=0; i<team.size(); i++){
+            if(team[i].second == false){
+                team[i].second = true;
+                comb[nth] = team[i].first;
+                sum  += cal_ability(team,nth+1, 0, points, comb);
+                // cout << "sum: " << sum << '\n';
+                team[i].second = false;
+            }
+        }
+        return sum;
+    }
+}
 
-// void calculate_diff(vector<bool>& people,vector<vector<int>>& points){
-//   vector<pair<int,bool>> team1;
-//   vector<pair<int,bool>> team2;
-//   for(int i=1; i<=n; i++){
-//     if(people[i]  == true) team1.push_back(pair<int,bool>(i,false));
-//     else{
-//       team2.push_back(pair<int,bool>(i,false));
-//     }
-//   }
-//   // cout << "team1\n";
-//   // for(auto p: team1){
-//   //   cout << "first: " << p.first << " second: " << p.second << '\n';
-//   // }
-//   // cout << "team2\n";
+void make_team(vector<bool>& teamMates, vector<vector<int> >& points, int nth, int prev){
+    //종료조건
+    if(nth == (teamMates.size())/2){
+        vector<int> s_comb(2);
+        vector<int> t_comb(2);
 
-//   // for(auto p: team2){
-//   //   cout << "first: " << p.first << " second: " << p.second << '\n';
-//   // }
+        // for(int i=0; i<teamMates.size(); i++){
+        //     cout << teamMates[i] << " ";
+        // }
+        // cout << '\n';
 
-//   int team1_point = 0;
-//   int team2_point = 0;
-  
-//   vector<int> comb_team1(2);
-//   vector<int> comb_team2(2);
+        vector<pair<int, bool> > team_s;
+        vector<pair<int, bool> > team_t;
 
-//   cal_point(team1,0,team1_point,comb_team1,points,0);
-//   cal_point(team2,0,team2_point,comb_team2,points,0);
-//   int point_diff = abs(team1_point - team2_point);
-//   diff.insert(point_diff);
-//   return ;
-// }
+        for(int i=0; i<teamMates.size(); i++){
+            if(teamMates[i] == true){
+                team_s.push_back(pair<int, bool>(i, false));
+            }
+            else{
+                team_t.push_back(pair<int,bool>(i,false));
+            }
+        }
+        // cout << "team_s: ";
+        // for (int i = 0; i < teamMates.size() / 2; i++)
+        // {
+        //     cout << team_s[i].first << " ";
+        // }
+        // cout << "\n";
 
-// void make_team(vector<bool>& people,int nth,vector<vector<int>>& points,int num){
-//   if(nth == n/2){
-//     // for(auto p: people){
-//     //   cout << p << " ";
-//     // }
-//     // cout << '\n';
-//     calculate_diff(people,points);
-//     return;
-//   }
-//   else{
-//     for(int i=num; i<=n; i++){
-//       if(people[i]==false){
-//         people[i] = true;
-//         make_team(people,nth+1,points,i+1);
-//         people[i] = false;
-//       }
-//     }
-//   }
-// }
+        // cout << "team_t: ";
+        // for (int i = 0; i < teamMates.size() / 2; i++)
+        // {
+        //     cout << team_t[i].first << " ";
+        // }
+        // cout << "\n";
+
+        int s_ability = cal_ability(team_s,0,0,points, s_comb);
+        // cout << "s_ability: " << s_ability << '\n';
+
+        int t_ability = cal_ability(team_t, 0,0,points, t_comb);
+        // cout << "t_ability: " << t_ability << '\n';
+
+        ability_diff.insert(abs(s_ability-t_ability));
+    }
+    else{
+        for(int i=prev; i<teamMates.size(); i++){
+            if(teamMates[i] == false){
+                teamMates[i] = true;
+                prev = i;
+                make_team(teamMates, points, nth+1, prev+1);
+                teamMates[i] = false;
+            }
+        }
+    }
+}
+
+void print_points(vector<vector<int> >& points){
+    for(int i=0 ; i<points.size(); i++){
+        for(int j=0; j<points[i].size(); j++){
+            cout << points[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
+
+int main(){
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    vector<bool> teamMates(N,false);
+    vector<vector<int> > points(N);
+
+    int y = 0;
+    while(N>0){
+        string line;
+        getline(cin,line);
+        istringstream iss(line);
+        int point;
+        while(iss>>point){
+            points[y].push_back(point);
+        }
+        N--;
+        y++;
+    }
+    //print_points(points);
 
 
+    //make_team
+    make_team(teamMates, points, 0, 0);
+    cout << *ability_diff.begin() << '\n';
 
-// int main(){
-
-//   cin >> n;
-//   vector<bool> people(n+1);
-//   vector<vector<int>> points(n);
-//   cin.ignore();
-//   int cycle = n;
-//   int y = 0;
-//   while(cycle>0){
-//     string line;
-//     getline(cin,line);
-//     istringstream iss{line};
-//     int point;
-//     while(iss>>point){
-//       points[y].push_back(point);
-//     }
-//     ++y;
-//     --cycle;
-//   }
-
-//   // for(int i=0; i<n; i++){
-//   //   for(int j=0; j<n; j++){
-//   //     cout << points[i][j] << " ";
-//   //   }
-//   //   cout << "\n";
-//   // }
-
-//   make_team(people,0,points,1);
-//   cout << *diff.begin() << '\n';
-// }
+}
