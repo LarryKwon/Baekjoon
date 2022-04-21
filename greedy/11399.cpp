1d8d13ac8e1f31286360789c5fe6a8866c84fb35
larrykwon <koallarry11@kaist.ac.kr>
2022-04-21 01:48:10 +0000

    웨이팅 문제. 프로세스에서 최소 시간 선택법으로 진행하듯, 정렬해서 더하면 됨.

---
+++ greedy/11399.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <memory>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <numeric>
+
+using namespace std;
+
+int cnt;
+vector<int> waits;
+vector<int> sums;
+
+
+int main()
+{
+    cin.tie(nullptr);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    cin >> cnt;
+    waits.resize(cnt);
+    for(int i=0; i<cnt; i++){
+        int wait;
+        cin >> wait;
+        waits[i] = wait;
+    }
+
+    sort(waits.begin(),waits.end());
+    int sum = 0;
+    for(int i=0; i<cnt; i++){
+        sum += waits[i];
+        waits[i] = sum;
+    }
+
+    // for(auto p: waits){
+    //     cout << p << ' ';
+    // }
+    
+    int result = accumulate(waits.begin(),waits.end(),0);
+    cout << result << '\n';
+}
