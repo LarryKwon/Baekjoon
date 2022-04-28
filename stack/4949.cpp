b23060a00f7baca7477678dbcf2d97a167bf9da9
laryrkwon <koallarry11@kaist.ac.kr>
2022-04-28 01:38:28 +0000

    4949번 괄호, (,{,[ 종류별로 다 찾기

--- stack/4949.cpp
+++ stack/4949.cpp
@@ -15,37 +15,58 @@
 
 using namespace std;
 
+bool is_parenthesis(char c){
+    if(c == '(' || c == ')' || c == '{' ||  c== '}' || c == '[' || c == ']') return true;
+    else return false;
+}
+
+bool complimentary(char c1, char c2){
+    if((c1 == '(' && c2 == ')')) return true;
+    else if(c1 == '{' && c2 == '}') return true;
+    else if(c1 == '[' && c2 == ']') return true;
+    else return false;
+}
+
 int main()
 {
-    // cin.tie(nullptr);
-    // cout.tie(NULL);
-    // ios_base::sync_with_stdio(false);
-
-    int n;
-    cin >> n;
-    cin.ignore();
-    for(int i=0; i<n; i++){
-        stack<char> parenthesis;
+    cin.tie(nullptr);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    while(true){    
+        stack<char> stacks;
         string line;
         getline(cin,line);
         istringstream iss{line};
         char c;
-        while(iss>>c){
-            if(c=='(') parenthesis.push(c);
-            else if(c==')') {
-                if(parenthesis.size()!=0){
-                    if(parenthesis.top()=='('){
-                        parenthesis.pop();
+        int i;
+        if(line.size()==1 && line[0]=='.') break;
+        for(i=0; i<line.size();i++){
+            c = line[i];
+            if(is_parenthesis(c)){
+                if(c == '(' || c == '{' || c=='[') stacks.push(c);
+                else{
+                    if(stacks.empty()){
+                        cout << "no\n";
+                        break;
                     }else{
-                        parenthesis.push(c);
+                        char top = stacks.top();
+                        if(complimentary(top,c)) stacks.pop();
+                        else{
+                            cout << "no\n";
+                            break;
+                        }
                     }
-                }else{
-                    parenthesis.push(c);
                 }
             }
         }
-        if(parenthesis.size()!=0) cout << "NO\n";
-        else cout << "YES\n";
+        if(i==line.size()){
+            if(stacks.size()==0) cout << "yes\n";
+            else{
+                cout << "no\n";
+            }
+        }
+        
+        
         cin.clear(ios_base::goodbit);
     }
 }
